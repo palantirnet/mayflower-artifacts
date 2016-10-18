5b2397f8b393a8015ce713c453e78837bcb67493
palantirnet-temp1 <devops@palantir.net>
2016-10-18 14:28:19 +0000

    Auto commit from tag 0.4.2

---
+++ assets/js/modules/bannerCarousel.js
@@ -0,0 +1,16 @@
+export default function (window,document,$,undefined) {
+
+  $('.js-banner-carousel').each(function(){
+    let $el = $(this);
+
+    if($el.children().length <= 1) {
+      return;
+    }
+
+    let slider = $el.slick({
+      prevArrow: '<button type="button" class="slick-prev"></button>',
+      nextArrow: '<button type="button" class="slick-next"></button>'
+    });
+  });
+
+}(window,document,jQuery);
